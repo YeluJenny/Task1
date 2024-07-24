8f60053db496add9d7a86967839213a873cf198b
yeluJenny <yelu4work@gmail.com>
2024-07-24 20:44:54 +0800

    task2

--- task2/bash.sh
+++ task2/bash.sh
@@ -1,5 +1,5 @@
-csv_file="ip-location.csv"
-
+#!/bin/bash
+csv_file="geoloc-Microsoft.csv"
 # Check if the file exists
 if [ ! -f "$csv_file" ]; then
     echo "The file $csv_file does not exist."
@@ -11,14 +11,13 @@ declare -A city_counts
 
 # Read the CSV file line by line
 while IFS=, read -r ip_range country region city; do
-    # Check if the country is China
+    #validate
     if [ "$country" == "CN" ]; then
         # Increment the city count
         ((city_counts[$city]++))
     fi
 done < "$csv_file"
-
-# Output the cities and their counts
+#output
 for city in "${!city_counts[@]}"; do
     echo "${city_counts[$city]} $city"
 done | sort -k1 -n
